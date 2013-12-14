5e563b5de3dbe78a93cac9fb89f5ad0ed5198dee
s1310629013 <s1310629013@students.fh-hagenberg.at>
2013-12-14 16:53:25 +0100

    CC

--- Assets/Scripts/CharacterControll1.cs
+++ Assets/Scripts/CharacterControll1.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-
 public class CharacterControll : MonoBehaviour {
-	
-	
-	
-	
+
+
+
+
 	//jump and crouch variables
 	CharacterController controller;
 	public float speed = 7f;
@@ -21,78 +20,73 @@ public class CharacterControll : MonoBehaviour {
 	public float crouchHeight = 1f;
 	public float standHight = 2f;
 	bool  isCrouched = false;
-	
+
 	//Tequila speed boost
 	public float speedBoost = 3f;
 	private int tequilaShots;
 	public GUIText shots;
 	bool speedBoosted = false;
 	float speedTimer = 2f;
-	float speedTimerMax = 0.5f;
-	
-	
-	
+
+
+
+
 	//game Start game Over
-	
-	
+
+
 	
 	void Start(){
-		
-		
+
+
 	}
-	
-	
+
+
 	void  Update (){
-		
+
 		shots.guiText.text = "Tequila: " + tequilaShots;
 		CharacterController controller = GetComponent<CharacterController>();
-		
+
 		Vector2 forward = transform.TransformDirection(Vector2.right);
 		controller.SimpleMove(forward * speed);
 		moveDirection.y -= gravity * Time.deltaTime;
 		controller.Move(moveDirection * Time.deltaTime);
-		
-		if (Input.GetKeyDown(KeyCode.Space) && isCrouched == false && controller.isGrounded)
-		{
-			moveDirection.y = jumpSpeed;
-			
-		}
+
+			if (Input.GetKeyDown(KeyCode.Space) && isCrouched == false && controller.isGrounded)
+			{
+				moveDirection.y = jumpSpeed;
+						
+			}
 		if (Input.GetKeyDown (KeyCode.LeftControl)) {
-			controller.center = new Vector2(0, 0);
-			controller.height = crouchHeight;
-			isCrouched = true;
-		}
-		if(Input.GetKeyUp (KeyCode.LeftControl) && isCrouched == true){
-			controller.center = new Vector2(0, 0.5f);
-			controller.height = standHight;
-			isCrouched = false;
-		}
+					controller.center = new Vector2(0, 0);
+					controller.height = crouchHeight;
+					isCrouched = true;
+				}
+		if (Input.GetKeyUp (KeyCode.LeftControl) && isCrouched == true){
+				controller.center = new Vector2(0, 0.5f);
+			   	controller.height = standHight;
+				isCrouched = false;
+			}
+
+		if (speedBoosted == true) {
+			speedTimer -= Time.deltaTime;
+			if(speedTimer <= Time.deltaTime){
+				speedBoosted = false;
+				speed = speed - 3;
+			}
+				}
+
 		if (Input.GetKeyDown (KeyCode.X)) {
-			
+
 			if(tequilaShots > 0)
 			{
 				speedBoosted = true;
-				speedTimer -= Time.deltaTime;
-				
-				
-				
 				speed = speed + 3f;
 				tequilaShots = tequilaShots - 1;
-				
-				if(speedTimer <= 0)
-				{
-					speedBoosted = false;
-					speed = speed - 3f;
-				}
-				
-				
 			}
-			
-			
-			
+				
 		}
-		
-	}
+
+}
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.tag == "Collectable")
@@ -100,10 +94,8 @@ public class CharacterControll : MonoBehaviour {
 			tequilaShots = tequilaShots + 1;
 		}
 	}
-	void Timer(){
-		speedTimer -= Time.deltaTime;
-	}
-	
+
+
 }
 
 
