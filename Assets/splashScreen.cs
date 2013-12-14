e627b39d41011b8ffb4a1c6c029f021e42b930ef
Ilias Koletsis <ilias.koletsis@gmail.com>
2013-12-15 00:51:14 +0100

    splash screen -> main menu -> level1

---
+++ Assets/splashScreen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class splashScreen : MonoBehaviour {
+
+	public Texture2D logoImage;
+
+	void OnGUI () {
+		// Make a background box
+		if (GUI.Button (new Rect (Screen.width/2,Screen.height/2,150,100), logoImage)) {
+			Application.LoadLevel(1);
+		}
+	}
+}
