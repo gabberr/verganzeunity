02371466f266699d94b7e23b86efdda49713a879
gabberr <gaber.setina@gmail.com>
2013-12-15 00:38:49 +0100

    no message

---
+++ Assets/GroundFactory.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundFactory : MonoBehaviour {
+
+	public GroundPartScript[] groundParts;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GenerateNextGround(Vector3 previous) {
+	//	groundFactory
+		GroundPartScript ground = Instantiate(groundParts[0], previous + Vector3.right * 11f, transform.rotation) as GroundPartScript;
+
+	
+	}
+
+}
