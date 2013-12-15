c02dc51bff13b6312cedc3b8a487002eb6645901
gabberr <gaber.setina@gmail.com>
2013-12-15 11:00:17 +0100

    Level generating part 1

--- Assets/Scripts/GroundFactory.cs
+++ Assets/Scripts/GroundFactory.cs
@@ -16,8 +16,9 @@ public class GroundFactory : MonoBehaviour {
 
 	public void GenerateNextGround(Vector3 previous) {
 	//	groundFactory
+	 	previous.y = 0;
 		int random = Random.Range(0, groundParts.Length);
-		GroundPartScript ground = Instantiate(groundParts[random], previous + Vector3.right * 11f, transform.rotation) as GroundPartScript;
+		GroundPartScript ground = Instantiate(groundParts[random], previous + Vector3.right * 14f, transform.rotation) as GroundPartScript;
 
 	
 	}
