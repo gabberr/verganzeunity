81b3a07d19f4977d280251b5cadface18e55302b
Ilias Koletsis <ilias.koletsis@gmail.com>
2013-12-15 12:59:50 +0100

    game win/over triggers

---
+++ Assets/GameWonScreen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameWonScreen : MonoBehaviour {
+
+	public Texture2D image;
+	
+	void OnGUI () {
+		GUI.Box(new Rect (0, 50, Screen.width, Screen.height), image);
+		StartCoroutine(MyMethod());
+	}
+	
+	IEnumerator MyMethod() {
+		yield return new WaitForSeconds(3);
+		Application.LoadLevel(2);
+	}
+}
