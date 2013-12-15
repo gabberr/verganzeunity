6f1f2758dec809313779e62605e88a86b96bb21b
Ilias Koletsis <ilias.koletsis@gmail.com>
2013-12-15 11:19:55 +0100

    short delay on splash screen

--- Assets/mainMenu.cs
+++ Assets/mainMenu.cs
@@ -5,17 +5,17 @@ public class mainMenu : MonoBehaviour {
 	public Texture2D menuImage;
 
 	void OnGUI () {
-		GUI.Box (new Rect (0,0,Screen.width,Screen.height), menuImage);
+		GUI.Box (new Rect (0, 50,Screen.width,Screen.height), menuImage);
 
-		if (GUI.Button (new Rect (Screen.width - 150, 20,107, 43), "")) {
+		if (GUI.Button (new Rect (Screen.width - 150, 70,107, 43), "")) {
 			Application.LoadLevel(1);
 		}
 
-		if (GUI.Button (new Rect (Screen.width - 135, 70,78, 43), "")) {
+		if (GUI.Button (new Rect (Screen.width - 135, 120,78, 43), "")) {
 			//Application.LoadLevel(1);
 		}
 
-		if (GUI.Button (new Rect (Screen.width - 155, 123,113, 43), "")) {
+		if (GUI.Button (new Rect (Screen.width - 155, 173,113, 43), "")) {
 			Application.Quit();
 		}
 	}
--- Assets/splashScreen.cs
+++ Assets/splashScreen.cs
@@ -6,10 +6,13 @@ public class splashScreen : MonoBehaviour {
 	public Texture2D logoImage;
 
 	void OnGUI () {
-		GUI.Box(new Rect (0, 0, Screen.width, Screen.height), logoImage);
-
-		if (GUI.Button (new Rect (Screen.width/2,Screen.height/2,150,100), "ENTER")) {
-			Application.LoadLevel(2);
-		}
+		GUI.Box(new Rect (0, 50, Screen.width, Screen.height), logoImage);
+		StartCoroutine(MyMethod());
 	}
+
+	IEnumerator MyMethod() {
+		yield return new WaitForSeconds(3);
+		Application.LoadLevel(2);
+	}
+
 }
