f346961ae28091f8d8fa7ff0e280056f6f63fd3d
Ilias Koletsis <ilias.koletsis@gmail.com>
2013-12-15 10:58:59 +0100

    no message

--- Assets/mainMenu.cs
+++ Assets/mainMenu.cs
@@ -10,5 +10,13 @@ public class mainMenu : MonoBehaviour {
 		if (GUI.Button (new Rect (Screen.width - 150, 20,107, 43), "")) {
 			Application.LoadLevel(1);
 		}
+
+		if (GUI.Button (new Rect (Screen.width - 135, 70,78, 43), "")) {
+			//Application.LoadLevel(1);
+		}
+
+		if (GUI.Button (new Rect (Screen.width - 155, 123,113, 43), "")) {
+			Application.Quit();
+		}
 	}
 }
